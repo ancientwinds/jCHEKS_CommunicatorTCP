572e74b47fe26874b326fbcd0ca12b5c1bf96f89
LepageT <thomas.lepage@hotmail.ca>
2015-06-17 09:07:29 -0400

    Remove print used for debugging.

--- src/com/archosResearch/jCHEKS/communicator/tcp/TCPSender.java
+++ src/com/archosResearch/jCHEKS/communicator/tcp/TCPSender.java
@@ -33,8 +33,7 @@ public class TCPSender extends AbstractSender{
 
             System.out.println("Sending communication to destination...");
             dataOutToDestination.write(communication.getCommunicationString().getBytes());
-            //dataOutToDestination.writeUTF(communication.getCommunicationString());
-            System.out.println(communication.getCommunicationString());
+
             // TODO : We should put a max waiting time for the ack to avoid hanging processes if the communication is lost
             // TODO : Then, we need to decide what we do if we don't receive the ack : to evolve, or not to evolve, that is the question!
             System.out.println("Waiting for ACK");
